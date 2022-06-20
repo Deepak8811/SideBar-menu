62b92ed9eba71f59b7090856fac9b531783bc6d7
Deepak <deepchaurasiya1997@gmail.com>
2022-06-21 01:40:59 +0530

    Final commit all work is done of this project

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const nevigation = document.querySelector(".nevigation");
 
 menuToggle.onclick = function () {
   nevigation.classList.toggle("open");
+};
+const list = document.querySelectorAll(".list");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
 }
+list.forEach((item) => item.addEventListener("click", activeLink));
